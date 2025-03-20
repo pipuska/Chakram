b91dd15525c3049c973b79bbf9d3c207518e005d
zik12 <zik12@KIBER_BOBR>
2025-03-20 20:08:05 +0700

    Добавьте файлы проекта.

---
+++ Chakram/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chakram.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
